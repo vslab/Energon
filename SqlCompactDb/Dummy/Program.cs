17babf5aa43a52da6ee29ee8d5934394c75e4b08
Davide Morelli <info@davidemorelli.it>
2012-03-27 17:20:54 +0200

    sql compact proj

---
+++ SqlCompactDb/Dummy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dummy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // dummy project
+            // needed onnly to have an IDE for editing Measurements.sdf
+        }
+    }
+}
