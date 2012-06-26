8d98c15318ae362c8a7ffce4d33df93a291ecd1f
unknown <root@HPLab.(none)>
2012-06-26 17:03:39 +0200

    function that creates csv with corr matrix

--- Testing/CorrMatrix.fsx
+++ Testing/CorrMatrix.fsx
@@ -89,204 +89,231 @@ let GetLinqContext =
 let db = GetLinqContext
 let exps = db.Experiments
 //exps.ToArray()
-let exp = db.Experiments.Where(fun (x:Experiments) -> x.Id = 4)
-let exp = db.Experiments.Where(fun (x:Experiments) -> x.Id = 7)
-let exp = db.Experiments.Where(fun (x:Experiments) -> x.Id = 11)
-let expCasesConvolution = db.ExperimentCases.Where(fun (x:ExperimentCases) -> x.Experiment_id = 4 )
-let expCasesConvolutionDGPU = db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
-    if x.Experiment_id = 4 then
-        let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
-        let mode = args.[0]
-        let ndev = args.[6]
-        let dev = args.[7]
-        if mode = "OPENCL" && ndev = "1" && dev = "0" then
-            true
+
+let createMatrix alg mode =
+    let exp = match alg with
+      | "convolution" -> db.Experiments.Where(fun (x:Experiments) -> x.Id = 4)
+      | "saxpy" -> db.Experiments.Where(fun (x:Experiments) -> x.Id = 7)
+      | "reduce" -> db.Experiments.Where(fun (x:Experiments) -> x.Id = 11)
+
+    let expCases = match (alg,mode) with
+      | "convolution", "DGPU" -> db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
+        if x.Experiment_id = 4 then
+            let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
+            let mode = args.[0]
+            let ndev = args.[6]
+            let dev = args.[7]
+            if mode = "OPENCL" && ndev = "1" && dev = "0" then
+                true
+            else
+                false
+        else
+            false    
+        )
+      | "convolution", "IGX" -> db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
+        if x.Experiment_id = 4 then
+            let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
+            let mode = args.[0]
+            let ndev = args.[6]
+            let dev = args.[7]
+            if mode = "OPENCL" && ndev = "1" && dev = "1" then
+                true
+            else
+                false
         else
             false
-    else
-        false    
-    )
-//expCasesConvolutionDGPU.Count()
-let expCasesConvolutionIGX = db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
-    if x.Experiment_id = 4 then
-        let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
-        let mode = args.[0]
-        let ndev = args.[6]
-        let dev = args.[7]
-        if mode = "OPENCL" && ndev = "1" && dev = "1" then
-            true
+        )
+      | "saxpy", "DGPU" -> db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
+        if x.Experiment_id = 7 then
+            let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
+            let mode = args.[0]
+            let ndev = args.[5]
+            let dev = args.[6]
+            if mode = "OPENCL" && ndev = "1" && dev = "0" then
+                true
+            else
+                false
+        else
+            false    
+        )
+      | "saxpy", "IGX" -> db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
+        if x.Experiment_id = 7 then
+            let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
+            let mode = args.[0]
+            let ndev = args.[5]
+            let dev = args.[6]
+            if mode = "OPENCL" && ndev = "1" && dev = "1" then
+                true
+            else
+                false
         else
             false
-    else
-        false
-    )
-
-let expCasesReduce = db.ExperimentCases.Where(fun (x:ExperimentCases) -> x.Experiment_id = 11 )
-let expCasesSaxpy = db.ExperimentCases.Where(fun (x:ExperimentCases) -> x.Experiment_id = 7 )
-
-let expCasesSaxpyDGPU = db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
-    if x.Experiment_id = 7 then
-        let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
-        let mode = args.[0]
-        let ndev = args.[5]
-        let dev = args.[6]
-        if mode = "OPENCL" && ndev = "1" && dev = "0" then
-            true
+        )
+      | "reduce", "DGPU" -> db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
+        if x.Experiment_id = 11 then
+            let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
+            let mode = args.[0]
+            let ndev = args.[5]
+            let dev = args.[6]
+            if mode = "OPENCL" && ndev = "1" && dev = "0" then
+                true
+            else
+                false
+        else
+            false    
+        )
+      | "reduce", "IGX" -> db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
+        if x.Experiment_id = 11 then
+            let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
+            let mode = args.[0]
+            let ndev = args.[5]
+            let dev = args.[6]
+            if mode = "OPENCL" && ndev = "1" && dev = "1" then
+                true
+            else
+                false
         else
             false
-    else
-        false    
-    )
-//expCasesConvolutionDGPU.Count()
-let expCasesSaxpyIGX = db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
-    if x.Experiment_id = 7 then
-        let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
-        let mode = args.[0]
-        let ndev = args.[5]
-        let dev = args.[6]
-        if mode = "OPENCL" && ndev = "1" && dev = "1" then
-            true
-        else
-            false
-    else
-        false
-    )
-
-let expCasesReduceDGPU = db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
-    if x.Experiment_id = 11 then
-        let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
-        let mode = args.[0]
-        let ndev = args.[5]
-        let dev = args.[6]
-        if mode = "OPENCL" && ndev = "1" && dev = "0" then
-            true
-        else
-            false
-    else
-        false    
-    )
-//expCasesConvolutionDGPU.Count()
-let expCasesReduceIGX = db.ExperimentCases.Where(fun (x:ExperimentCases) -> 
-    if x.Experiment_id = 11 then
-        let args = x.Args.Split([| ";" |], StringSplitOptions.RemoveEmptyEntries)
-        let mode = args.[0]
-        let ndev = args.[5]
-        let dev = args.[6]
-        if mode = "OPENCL" && ndev = "1" && dev = "1" then
-            true
-        else
-            false
-    else
-        false
-    )
+        )
 
 
-let expCases = expCasesReduceDGPU
-let expCases = expCasesReduceIGX
-//let expCases = expCasesSaxpyDGPU
-//let expCases = expCasesSaxpyIGX
-let expCases = expCasesConvolutionDGPU
-let expCases = expCasesConvolutionIGX
-//let expCases = expCasesReduce
-//let expCases = expCasesSaxpy
-//let expCases = expCasesConvolution
+    let expRuns (case:ExperimentCases) =
+        db.ExperimentRuns.Where(fun (x:ExperimentRuns) -> x.Experiment_case_id = case.Id)
 
-let arg1 (case:ExperimentCases) =
-    let tags = case.Args.Split([|";"|], StringSplitOptions.RemoveEmptyEntries)
-    tags.[0]
+    let handleRun (x:ExperimentRuns) =
+        let sensors = db.Sensors.Where(fun (s:Sensors) -> s.Experiment_run_id = x.Id).OrderBy(fun (s:Sensors) -> s.Sensor_class_id) |> Seq.toArray
+        let getSensorClass (s:Sensors) =
+            db.SensorClasses.First(fun (c:SensorClasses) -> c.Id = s.Sensor_class_id)
+        let getReadings (s:Sensors) =
+            //let measures = db.Measurements1.Where(fun (m:Measurements1) -> m.Sensor_id = s.Id )
+            //measures |> Seq.map (fun (m:Measurements1) -> m.Value) |> Seq.average
+            let measures = db.Measurements.Where(fun (m:Measurements) -> m.Sensor_id = s.Id )
+            measures |> Seq.map (fun (m:Measurements) -> m.Value) |> Seq.average
+        let vals =
+            Seq.map getReadings sensors
+        (sensors.ToArray(), vals)
 
-let expRuns (case:ExperimentCases) =
-    db.ExperimentRuns.Where(fun (x:ExperimentRuns) -> x.Experiment_case_id = case.Id)
+    let handleCase (case:ExperimentCases) =
+        let run = (expRuns case).First() // TODO
+        let s,v = handleRun run
+        let args = case.Args.Split([|";"|], StringSplitOptions.RemoveEmptyEntries)
+        let firstArg = match args.[0] with
+                        | "HOST_SEQ" -> 0.
+                        | "HOST_PAR" -> 1.
+                        | "OPENCL" -> 2.
+                        | _ -> -1.
+        let argsToFloatSeq = seq {
+                yield firstArg
+                let argN = args.Length - 1
+                for i in 1..argN do
+                    yield float(System.Single.Parse(args.[i]))
+            }
+        let argArray = argsToFloatSeq.ToArray()
+        let valArray = v.ToArray()
+        let j = valArray.[0] * valArray.[1]
+        match alg with
+        | "convolution" -> 
+            let mat_size = argArray.[1] * argArray.[2]
+            Array.concat [| argArray; valArray; [| j ; mat_size |] |]
+        | _ -> Array.concat [| argArray; valArray; [| j; argArray.[1] |] |]
+        //let mat_size = argArray.[1] * argArray.[2]
+        //Array.concat [| argArray; valArray; [| j ; mat_size |] |]
 
-let handleRun (x:ExperimentRuns) =
-    let sensors = db.Sensors.Where(fun (s:Sensors) -> s.Experiment_run_id = x.Id).OrderBy(fun (s:Sensors) -> s.Sensor_class_id) |> Seq.toArray
-    let getSensorClass (s:Sensors) =
-        db.SensorClasses.First(fun (c:SensorClasses) -> c.Id = s.Sensor_class_id)
-    let getReadings (s:Sensors) =
-        //let measures = db.Measurements1.Where(fun (m:Measurements1) -> m.Sensor_id = s.Id )
-        //measures |> Seq.map (fun (m:Measurements1) -> m.Value) |> Seq.average
-        let measures = db.Measurements.Where(fun (m:Measurements) -> m.Sensor_id = s.Id )
-        measures |> Seq.map (fun (m:Measurements) -> m.Value) |> Seq.average
-    let vals =
-        Seq.map getReadings sensors
-    (sensors.ToArray(), vals)
+    let data cases = cases |> Seq.map (fun (c:ExperimentCases) -> (handleCase c))
 
-let handleCase (case:ExperimentCases) =
-    let run = (expRuns case).First()
-    let s,v = handleRun run
-    let args = case.Args.Split([|";"|], StringSplitOptions.RemoveEmptyEntries)
-    let firstArg = match args.[0] with
-                    | "HOST_SEQ" -> 0.
-                    | "HOST_PAR" -> 1.
-                    | "OPENCL" -> 2.
-                    | _ -> -1.
-    let argsToFloatSeq = seq {
-            yield firstArg
-            let argN = args.Length - 1
-            for i in 1..argN do
-                yield float(System.Single.Parse(args.[i]))
-        }
-    let argArray = argsToFloatSeq.ToArray()
-    let valArray = v.ToArray()
-    let j = valArray.[0] * valArray.[1]
-    let mat_size = argArray.[1] * argArray.[2]
-    Array.concat [| argArray; valArray; [| j ; mat_size |] |]
+    let colNames (e:Experiments) (c:ExperimentCases) (r:ExperimentRuns)=
+        let args = e.ArgNames.Split([|";"|], StringSplitOptions.RemoveEmptyEntries)
+        let run = (expRuns c).First() // TODO
+        let sensors,v = handleRun run
+        //let sensors = db.Sensors.Where(fun (s:Sensors) -> s.Experiment_run_id = r.Id)
+        let sensorName (s:Sensors) = 
+            db.SensorClasses.Where(fun (cl:SensorClasses) -> cl.Id = s.Sensor_class_id).First().SensorName
 
-let data cases =
-    cases |> Seq.map (fun (c:ExperimentCases) -> (handleCase c))
+        let sensorsNames = (Seq.map sensorName sensors).ToArray()
+        Array.concat [| args ; sensorsNames ; [|"J"; "vector_size"|] |]
+
+    let names = colNames (exp.First()) (expCases.First()) (Seq.head (expRuns (expCases.First())))
+
+    let cols = names.Count()
+
+    let colIdx name = 
+        let mutable res = -1
+        let namesL = names.Length - 1 
+        for i in 0..namesL do
+            if name = names.[i] then
+                res <- i
+        res
+
+    let colName idx = names.[idx]
+
+    let casesSubset = expCases
+    //let casesSubset = expCases.Skip (round*11) |> Seq.take 11
+    let valuesMatrix = data casesSubset
+    let vals = valuesMatrix.ToArray()
+    let corrMatr = getCorrMatrix vals
+    let rows = names.Length - 1
+    //let interestingIdx = [| 1;2;7;8;9;16;17;18;19;20;21;22;23;24;25;26;27;28;29;30;31;32;33;34;35;36;37;38 |]
+    //let interestingIdx = [| 1;2;7;8;9;16;17;18;22;23;26;27;28;29;30;35 |]
+    //let interestingIdx = [| 1..36 |]
+    let interestingIdx = match mode with
+        | "DGPU" -> [| 
+            colIdx "vector_size"; 
+            colIdx "extechWatt"; 
+            colIdx "completionTime"; 
+            colIdx "J"; 
+            colIdx "d0_mode_in"; 
+            colIdx "d0_mode_out"; 
+            colIdx "gpuBusy_1"; 
+            colIdx "waveFronts_1"; 
+            colIdx "AluBusy_1"; 
+            colIdx "aluPacking_1"; 
+            colIdx "fetchUnitBusy_1"; 
+            colIdx "fetchUnitStalled_1"; 
+            colIdx "fastPath_1"; 
+            |]
+        | "IGX" -> [| 
+            colIdx "vector_size"; 
+            colIdx "extechWatt"; 
+            colIdx "completionTime"; 
+            colIdx "J"; 
+            colIdx "d0_mode_in"; 
+            colIdx "d0_mode_out"; 
+            colIdx "gpuBusy_2"; 
+            colIdx "waveFronts_2"; 
+            colIdx "AluBusy_2"; 
+            colIdx "aluPacking_2"; 
+            colIdx "fetchUnitBusy_2"; 
+            colIdx "fetchUnitStalled_2"; 
+            colIdx "fastPath_2"; 
+            |]
+    //let interestingIdx = [| 1..(rows-1) |]
+    let sb = new System.Text.StringBuilder()
+    sb.Append(" ;") |> ignore
+    //names |> Seq.iter (fun (s:string) -> sb.AppendFormat(@"{0};", s) |> ignore)
+    interestingIdx |> Seq.iter (fun i -> sb.AppendFormat(@"{0};", colName (i)) |> ignore)
+
+    for i in interestingIdx do
+        sb.AppendLine("") |> ignore
+        sb.AppendFormat(@"{0};", colName (i)) |> ignore
+        for j in interestingIdx do
+            sb.AppendFormat(@"{0};", corrMatr.[i,j]) |> ignore
+
+    match (alg,mode) with
+      | "convolution", "DGPU" -> System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\convolution_DGPU_corrMatr.csv", sb.ToString())
+      | "convolution", "IGX" -> System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\convolution_IGX_corrMatr.csv", sb.ToString())
+      | "saxpy", "DGPU" -> System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\saxpy_DGPU_corrMatr.csv", sb.ToString())
+      | "saxpy", "IGX" -> System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\saxpy_IGX_corrMatr.csv", sb.ToString())
+      | "reduce", "DGPU" -> System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\reduce_DGPU_corrMatr.csv", sb.ToString())
+      | "reduce", "IGX" -> System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\reduce_IGX_corrMatr.csv", sb.ToString())
+      | _,_ -> ()
 
 
-let colNames (e:Experiments) (c:ExperimentCases) (r:ExperimentRuns)=
-    let args = e.ArgNames.Split([|";"|], StringSplitOptions.RemoveEmptyEntries)
-    let sensors = db.Sensors.Where(fun (s:Sensors) -> s.Experiment_run_id = r.Id)
-    let sensorName (s:Sensors) = 
-        db.SensorClasses.Where(fun (cl:SensorClasses) -> cl.Id = s.Sensor_class_id).First().SensorName
-    let sensorsNames = (Seq.map sensorName sensors).ToArray()
-    Array.concat [| [|" "|] ;args ; sensorsNames ; [|"J"; "matrix_size"|] |]
-
-let names = colNames (exp.First()) (expCases.First()) (Seq.head (expRuns (expCases.First())))
-
-let cols = names.Count()
-
-//names.ToArray().Length
-//exp.First().ArgNames.Split([|";"|], StringSplitOptions.RemoveEmptyEntries).Length
-//names.Count()
-
-let colIdx name = 
-    let mutable res = -1
-    let namesL = names.Length - 1 
-    for i in 0..namesL do
-        if name = names.[i] then
-            res <- i
-    res
-
-let colName idx = names.[idx]
-
-
-//sb.AppendLine("")
-
-
-let casesSubset = expCases
-//let casesSubset = expCases.Skip (round*11) |> Seq.take 11
-let valuesMatrix = data casesSubset
-let vals = valuesMatrix.ToArray()
-let corrMatr = getCorrMatrix vals
-let rows = names.Length - 1
-//let interestingIdx = [| 1;2;7;8;9;16;17;18;19;20;21;22;23;24;25;26;27;28;29;30;31;32;33;34;35;36;37;38 |]
-let interestingIdx = [| 1;2;7;8;9;16;17;18;22;23;26;27;28;29;30;35 |]
-let interestingIdx = [| 1..36 |]
-
-let sb = new System.Text.StringBuilder()
-sb.Append(" ;");
-//names |> Seq.iter (fun (s:string) -> sb.AppendFormat(@"{0};", s) |> ignore)
-interestingIdx |> Seq.iter (fun i -> sb.AppendFormat(@"{0};", colName (i+1)) |> ignore)
-
-for i in interestingIdx do
-    sb.AppendLine("") |> ignore
-    sb.AppendFormat(@"{0};", colName (i+1)) |> ignore
-    for j in interestingIdx do
-        sb.AppendFormat(@"{0};", corrMatr.[i,j]) |> ignore
-
+createMatrix "convolution" "DGPU"
+createMatrix "convolution" "IGX"
+createMatrix "saxpy" "DGPU"
+createMatrix "saxpy" "IGX"
+createMatrix "reduce" "DGPU"
+createMatrix "reduce" "IGX"
 
 
 1+1
@@ -299,14 +326,7 @@ sb.ToString()
 //System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\convolution_correlations.csv", sb.ToString())
 //System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\convolution_correlations_text.csv", sb2.ToString())
 
-System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\convolution_DGPU_corrMatr.csv", sb.ToString())
-System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\convolution_IGX_corrMatr.csv", sb.ToString())
-
-System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\saxpy_DGPU_corrMatr.csv", sb.ToString())
-System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\saxpy_IGX_corrMatr.csv", sb.ToString())
-
-System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\reduce_DGPU_corrMatr.csv", sb.ToString())
-System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\reduce_IGX_corrMatr.csv", sb.ToString())
+System.IO.File.WriteAllText(@"C:\Users\root\Desktop\Energon\Measures\corrMatr.csv", sb.ToString())
 
 // --------------------  print values ---------------------
 
