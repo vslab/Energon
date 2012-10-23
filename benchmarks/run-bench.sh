878fcfb1c293e949118dda6a3a006fe70d1ab70f
Davide Morelli <info@davidemorelli.it>
2012-10-24 00:17:06 +0200

    handling args

--- benchmarks/run-bench.sh
+++ benchmarks/run-bench.sh
@@ -8,12 +8,24 @@
 # create new experiment
 # $1 is the algo
 # $2 is the array size
-
+PROGR=$1
+INSIZE=$2
+PERFARGS=" "
+MINUSE=" -e "
+let count=0
+for var in "$@"
+do
+    if [ $count -gt 1 ]; then
+        PERFARGS=$PERFARGS$MINUSE$var
+    fi
+    ((count++))
+done
+echo $PERFARGS
 # call run (to start the ammeter)
 
 # run the program...
-var=`perf stat -e cycles -e instructions -e cache-misses -e cpu-clock -e branch-misses  ./$1 $2 2>&1`
-
+perfout=`perf stat $PERFARGS ./$PROGR $INSIZE 2>&1`
+echo $perfout
 # parse the output of perf stat
 
 # call stop (to stop the ammeter)
