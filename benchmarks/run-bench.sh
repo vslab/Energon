b0981a40de53d4dbbcb512e01d3ae9953dc8e92d
Davide Morelli <info@davidemorelli.it>
2012-10-24 13:50:20 +0200

    parsing perf counters from perf stat output

--- benchmarks/run-bench.sh
+++ benchmarks/run-bench.sh
@@ -13,20 +13,51 @@ INSIZE=$2
 PERFARGS=" "
 MINUSE=" -e "
 let count=0
+let countParms=0
+declare -a perfcounters
 for var in "$@"
 do
     if [ $count -gt 1 ]; then
         PERFARGS=$PERFARGS$MINUSE$var
+        perfcounters[$countParms]=$var
+        ((countParms++))
     fi
     ((count++))
 done
-echo $PERFARGS
+
 # call run (to start the ammeter)
 
 # run the program...
 perfout=`perf stat $PERFARGS ./$PROGR $INSIZE 2>&1`
-echo $perfout
+
 # parse the output of perf stat
+ll=($perfout)
+count=0
+declare -a res
+divisor=":"
+for w in ${ll[@]}; 
+do 
+  if [ $count -ge 1 ]; then
+    #echo $count$divisor$w;
+    n=${ll[$count]}
+    # is this word a counter name?
+    localcounter=0
+    for perfcounter in ${perfcounters[@]};
+    do
+      if [ "$n" == "$perfcounter" ]; then
+        # the current word is a perf counter name
+        # the preceding word was its value
+        res[localcounter]=${ll[$count-1]}
+      fi
+      ((localcounter++))
+    done  
+  fi
+  ((count++))
+done
+
+echo ${perfcounters[@]}
+echo ${res[@]}
+
 
 # call stop (to stop the ammeter)
 
