b63d0b98d15b6d8c138e465614d6ba7e2d47a104
Davide Morelli <info@davidemorelli.it>
2012-10-24 18:27:17 +0200

    stub to chose perf counters

--- benchmarks/run-bench.sh
+++ benchmarks/run-bench.sh
@@ -10,9 +10,40 @@
 
 PROGR=$1
 INSIZE=$2
+REMOTE=$3
+
 cd bin
 
-function selectCase {
+declare -a availPerfCounters
+
+function setPerformanceCounters {
+  perfout=`perf list hw 2>&1`
+  ll=($perfout)
+  count=0
+  declare -a res
+  divisor=":"
+  for w in ${ll[@]}; 
+  do
+    if [[ $w != "[Hardware" ]] && [[ $w != "event]" ]] ; then
+      if [[ $w == "OR" ]] ; then
+        ((count--))
+        ((count--))
+      else
+        availPerfCounters[$count]=$w
+      fi
+    fi
+    ((count++))
+  done
+  echo ${availPerfCounters[@]}
+}
+
+function setRemoteIP {
+  echo "Select the IP for the remote host (running the energon measurement framework)"
+  read REMIP
+  REMOTE=$REMIP
+}
+
+function selectProgram {
   echo "Select the program to measure:"
   select FILENAME in *;
   do
@@ -31,62 +62,104 @@ function selectInputSize {
   done
 }
 
-selectCase
-selectInputSize
-echo ciao
-
-PERFARGS=" "
-MINUSE=" -e "
-let count=0
-let countParms=0
-declare -a perfcounters
-for var in "$@"
-do
+function getPerfFromArgs {
+  PERFARGS=" "
+  MINUSE=" -e "
+  let count=0
+  let countParms=0
+  declare -a perfcounters
+  for var in "$@"
+  do
     if [ $count -gt 1 ]; then
         PERFARGS=$PERFARGS$MINUSE$var
         perfcounters[$countParms]=$var
         ((countParms++))
     fi
     ((count++))
-done
+  done
+}
 
 # create new experiment
-
+function newExperiment {
+  echo "TODO: newExperiment"
+}
 
 # call run (to start the ammeter)
-
+function callRun {
+  echo "TODO: callRun"
+}
 
 # run the program...
-perfout=`perf stat $PERFARGS ./$PROGR $INSIZE 2>&1`
-
-# parse the output of perf stat
-ll=($perfout)
-count=0
-declare -a res
-divisor=":"
-for w in ${ll[@]}; 
-do 
-  if [ $count -ge 1 ]; then
-    #echo $count$divisor$w;
-    n=${ll[$count]}
-    # is this word a counter name?
-    localcounter=0
-    for perfcounter in ${perfcounters[@]};
-    do
-      if [ "$n" == "$perfcounter" ]; then
-        # the current word is a perf counter name
-        # the preceding word was its value
-        res[localcounter]=${ll[$count-1]}
-      fi
-      ((localcounter++))
-    done  
-  fi
-  ((count++))
-done
-
-echo ${perfcounters[@]}
-echo ${res[@]}
-
+function runProgram {
+  echo "running $PROGR($INSIZE)..."
+  perfout=`perf stat $PERFARGS ./$PROGR $INSIZE 2>&1`
+  # parse the output of perf stat
+  ll=($perfout)
+  count=0
+  declare -a res
+  divisor=":"
+  for w in ${ll[@]}; 
+  do 
+    if [ $count -ge 1 ]; then
+      #echo $count$divisor$w;
+      n=${ll[$count]}
+      # is this word a counter name?
+      localcounter=0
+      for perfcounter in ${perfcounters[@]};
+      do
+        if [ "$n" == "$perfcounter" ]; then
+          # the current word is a perf counter name
+          # the preceding word was its value
+          res[localcounter]=${ll[$count-1]}
+        fi
+        ((localcounter++))
+      done  
+    fi
+    ((count++))
+  done
+  echo ${perfcounters[@]}
+  echo ${res[@]}
+}
 
 # call stop (to stop the ammeter)
+function callStop {
+  echo "TODO: callStop"
+}
+
+clear
+echo "This is the energon benchmark framework"
+
+quit="no"
+while [ $quit != "yes" ]
+do
+echo ""
+echo "currently remote IP is $REMOTE"
+echo "selected program is $PROGR"
+echo "input size is $INSIZE"
+echo ""
+echo "1. run"
+echo "2. set program"
+echo "3. set input size"
+echo "4. set remote host ip"
+echo "5. set performance counters"
+echo "6. exit"
+echo -n "Your choice? : "
+read choice
+
+case $choice in
+1)
+  newExperiment
+  callRun
+  runProgram
+  callStop
+  ;;
+2) selectProgram ;;
+3) selectInputSize ;;
+4) setRemoteIP ;;
+5) setPerformanceCounters ;;
+6) quit="yes" ;;
+*) echo "\"$choice\" is not valid"
+esac
+done
+
 
