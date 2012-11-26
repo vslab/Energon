d92f1f31d74ea21d222858946ff4d5c1ad051f4b
unknown <Davide@Davide-Dell.parser.it>
2012-11-26 19:22:41 +0100

    script for win

--- Testing/Script.fsx
+++ Testing/Script.fsx
@@ -3,6 +3,79 @@
 #r @"Energon.Measuring.dll"
 open Energon.Measuring
 
+let procTest1 = new PerfCounter("Processor", "% User Time", 1.)
+procTest1.Instance <- "_Total"
+let procTest2 = new PerfCounter("Memory", "Cache Faults/sec", 1.)
+
+let runExp sensors exp =
+    let run (s:PullSensor) =
+        s.Start()
+    let halt (s:PullSensor) =
+        s.Stop()
+    let halt (s:PullSensor) =
+        s.Stop()
+    let getAvg (rl:seq<Reading>) =
+        let asList = Seq.toList rl
+        let sum = Seq.fold (fun state (reading:Reading)-> state + reading.Value ) 0. rl
+        let n = float(List.length asList)
+        sum / n
+    let getRes (s:PushSensor) =
+        getAvg s.GetResultsAndReset
+    Seq.iter run sensors
+    exp()
+    Seq.iter halt sensors
+    Seq.map getRes sensors
+
+
+let runProg (prog:string) args =
+    let psi = new System.Diagnostics.ProcessStartInfo(System.String.Format("C:\\progetti\\Energon\\benchmarks\\{0}.exe", prog))
+    psi.RedirectStandardOutput <- true
+    psi.WindowStyle <- System.Diagnostics.ProcessWindowStyle.Hidden
+    psi.UseShellExecute <- false
+    psi.Arguments <- args
+    let proc = System.Diagnostics.Process.Start(psi)
+    let myOut = proc.StandardOutput
+    proc.WaitForExit()
+
+let helper = new Energon.Measuring.Remote.RemoteSensorHelper("131.114.88.115")
+let run prog arg = 
+    let load() = 
+        runProg prog arg
+    let starttime = System.DateTime.Now
+    helper.start() |> ignore
+    let vals = Seq.map (fun (f:float) -> System.String.Format(System.Globalization.CultureInfo.InvariantCulture.NumberFormat, "{0}", f)) (runExp [| procTest1; procTest2 |] load)
+    let stoptime = System.DateTime.Now
+    let vals2 = seq {
+            yield System.String.Format(System.Globalization.CultureInfo.InvariantCulture.NumberFormat, "{0}",stoptime.Subtract(starttime).TotalSeconds)
+        }
+    let vals3 = Seq.append vals vals2
+    helper.stop(vals3) |> ignore
+
+let runCase prog arg = 
+    let args = seq {yield arg}
+    System.Console.WriteLine(System.String.Format("preparing to run program {0} with arg {1}", prog, arg))
+    (helper.experimentCase args) |> ignore
+    for i in 1..5 do
+        System.Console.WriteLine(System.String.Format("running iteration {0}", i))
+        run prog arg
+
+let args = [| "1048576"; "4194304"; "16777216"; "67108864"; "268435456"; "1073741824" |]
+
+runCase "quick" args.[3]
+
+let runCycle prog =
+    args |> Seq.iter (fun a -> runCase prog a )
+
+
+runCycle "quick"
+runCycle "merges"
+runCycle "heap"
+
+runCase "randMemAccess" args.[3]
+runCase "simpleINT" args.[3]
+runCase "simpleFPU" args.[3]
+
+
 (*
 // a couple of sensors...
 let proc = new PerfCounter("Process", "% Processor Time", 1.)
