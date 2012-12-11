4b65ad58903fc7ef3eae2249eb376980d42daf98
unknown <Davide@Davide-Dell.parser.it>
2012-12-11 22:40:04 +0100

    LinearProgramming

--- LinearProgramming/Script.fsx
+++ LinearProgramming/Script.fsx
@@ -2,20 +2,23 @@
 // It can be used to explore and test the library project.
 // Note that script files will not be part of the project build.
 
-#I "C:\Users\root\Desktop\Energon\LinearProgramming"
-#r @"GlpkSharp.dll"
+#I @"C:\Users\root\Desktop\Energon\bin\Debug"
+#I @"C:\progetti\Energon\bin\Debug"
+#r @"GLPKSharp.dll"
 
-open GlpkSharp
+//#I "C:\Users\root\Desktop\Energon\LinearProgramming"
+//#I "C:\progetti\Energon\LinearProgramming"
+//#r @"GlpkSharp.dll"
 
-let prob = new LPProblem()
-prob.AddCols(2)
-prob.AddRows(2)
-prob.ObjectiveDirection = OptimisationDirection.MAXIMISE
+let prob = new GLPKSharp.GLPKSharp()
 
-let ia : int array = Array.zeroCreate 4
-let ja : int array = Array.zeroCreate 4
-let a : float array = Array.zeroCreate 4
-let c = ref 0
+prob.AddCols(3)
+prob.AddRows(3)
+
+let ia : int array = Array.zeroCreate 10
+let ja : int array = Array.zeroCreate 10
+let a : float array = Array.zeroCreate 10
+let c = ref 1
 let setMatCell i j v =
     ia.[!c] <- i
     ja.[!c] <- j
@@ -24,27 +27,77 @@ let setMatCell i j v =
 
 setMatCell 1 1 1.
 setMatCell 1 2 1.
-setMatCell 2 1 1.
-setMatCell 2 2 2.
+setMatCell 1 3 1.
 
-//prob.SetMatRow(1, [| 1; 2; |], [| 2.; 2. |]);
-//prob.SetMatRow(2, [| 1; 2; |], [| 3.; 1. |]);
+setMatCell 2 1 10.
+setMatCell 2 2 4.
+setMatCell 2 3 5.
+
+setMatCell 3 1 2.
+setMatCell 3 2 2.
+setMatCell 3 3 6.
 
 prob.LoadMatrix(ia, ja, a)
-prob.SetRowBounds(1, BOUNDSTYPE.Fixed, 2., 2.)
-prob.SetRowBounds(2, BOUNDSTYPE.Fixed, 4., 4.)
-prob.SetColBounds(1, BOUNDSTYPE.Double, 0., 2.)
-prob.SetColBounds(2, BOUNDSTYPE.Double, 2., 4.)
-prob.SetColName(1, "pippo")
-prob.SetColName(2, "pluto")
+
+prob.SetRowBnds(1, GLPKSharp.Boundary.UpperBound, 0., 100.)
+prob.SetRowBnds(2, GLPKSharp.Boundary.UpperBound, 0., 600.)
+prob.SetRowBnds(3, GLPKSharp.Boundary.UpperBound, 0., 300.)
+
+prob.SetObjCoef(1, 10.)
+prob.SetObjCoef(2, 6.)
+prob.SetObjCoef(3, 4.)
+
+prob.SetObjectiveDirection(GLPKSharp.ObjectiveDirection.Minimize)
+
+prob.Simplex()
+
+prob.GetObjVal()
+
+prob.GetColPrim(1)
+prob.GetColPrim(2)
+prob.GetColPrim(3)
+
+
+
+
+// ---------------------------- 
+
+let prob = new GLPKSharp.GLPKSharp()
+
+prob.AddCols(2)
+prob.AddRows(1)
+
+let ia : int array = Array.zeroCreate 3
+let ja : int array = Array.zeroCreate 3
+let a : float array = Array.zeroCreate 3
+let c = ref 1
+let setMatCell i j v =
+    ia.[!c] <- i
+    ja.[!c] <- j
+    a.[!c] <- v
+    c := !c + 1
+
+setMatCell 1 1 1.
+setMatCell 1 2 1.
+
+prob.LoadMatrix(ia, ja, a)
+
+prob.SetRowBnds(1, GLPKSharp.Boundary.UpperBound, 0., 2.)
+
 prob.SetObjCoef(1, 1.)
 prob.SetObjCoef(2, 1.)
-prob.SolveSimplex()
 
-prob.GetObjectiveValue()
-prob.GetColPrimal(1)
-prob.GetColPrimal(2)
+prob.SetObjectiveDirection(GLPKSharp.ObjectiveDirection.Maximize)
 
-prob.WriteSol("c:\\test.txt")
+prob.Simplex()
+
+prob.GetObjVal()
+
+prob.GetColPrim(1)
+prob.GetColPrim(2)
+prob.GetColPrim(3)
+
+
+
 
 
---
+++ LinearProgramming/glpk.fsx
@@ -0,0 +1,59 @@
+﻿#I @"C:\Users\root\Desktop\Energon\bin\Debug"
+#I @"C:\progetti\Energon\bin\Debug"
+#r @"testClass.dll"
+
+#r @"GlpkSharp.dll"
+
+
+open GlpkSharp
+
+open testClass
+
+let t = new Class1()
+t.test2()
+
+let rowbounds0 = new testClass.Class1.bound(BoundType = BOUNDSTYPE.Upper, lower = 0., upper=100.)
+let rowbounds1 = new testClass.Class1.bound(BoundType = BOUNDSTYPE.Upper, lower = 0., upper=600.)
+let rowbounds2 = new testClass.Class1.bound(BoundType = BOUNDSTYPE.Upper, lower = 0., upper=300.)
+let rowbouds = [| rowbounds0; rowbounds1; rowbounds2 |]
+
+let colbounds0 = new testClass.Class1.bound( BoundType = BOUNDSTYPE.Lower, lower = 0., upper = 0. )
+let colbounds1 = new testClass.Class1.bound( BoundType = BOUNDSTYPE.Lower, lower = 0., upper = 0. )
+let colbounds2 = new testClass.Class1.bound( BoundType = BOUNDSTYPE.Lower, lower = 0., upper = 0. )
+let colbouds = [| colbounds0; colbounds1; colbounds2 |]
+
+
+let ia = [| 0; 1; 1; 1; 2; 2; 2; 3; 3; 3 |]
+let ja = [| 0; 1; 2; 3; 1; 2; 3; 1; 2; 3 |]
+let ar = [| 0.; 1.; 1.; 1.; 10.; 4.; 5.; 2.; 2.; 6. |]
+
+let coeff = [|10.; 6.; 4. |]
+
+t.Simplex(3, 3, OptimisationDirection.MAXIMISE, rowbouds, colbouds, ia, ja, ar, coeff);
+
+
+
+
+let p = new LPProblem()
+p.ObjectiveDirection = OptimisationDirection.MAXIMISE
+p.AddCols(3)
+p.AddRows(3)
+p.SetRowBounds(1, BOUNDSTYPE.Upper, 0., 100.)
+p.SetRowBounds(2, BOUNDSTYPE.Upper, 0., 600.)
+p.SetRowBounds(3, BOUNDSTYPE.Upper, 0., 300.)
+p.SetColBounds(1, BOUNDSTYPE.Lower, 0., 0.)
+p.SetColBounds(2, BOUNDSTYPE.Lower, 0., 0.)
+p.SetColBounds(3, BOUNDSTYPE.Lower, 0., 0.)
+let ia = [| 0; 1; 1; 1; 2; 2; 2; 3; 3; 3|]
+let ja = [| 0; 1; 2; 3; 1; 2; 3; 1; 2; 3|]
+let ar = [| 0.; 1.; 1.; 1.; 10.; 4.; 5.; 2.; 2.; 6. |]
+p.LoadMatrix(ia, ja, ar)
+//p.SetMatRow(1, new int[] {0, 1, 2 }, new double[] {0, 1, 1 });
+p.SetObjCoef(1, 10.)
+p.SetObjCoef(2, 6.)
+p.SetObjCoef(3, 4.)
+p.SolveSimplex()
+
+open System
+Console.WriteLine("result = {0}, x1 = {1}, x2 = {2}", p.GetObjectiveValue(), p.GetColPrimal(1), p.GetColPrimal(2))
+Console.In.ReadLine()
