6f467c1e4af044e456c2a3b10fae211397a65344
Davide Morelli <info@davidemorelli.it>
2013-04-15 13:02:37 +0000

    detect perf version

--- benchmarks/run-bench.sh
+++ benchmarks/run-bench.sh
@@ -31,6 +31,13 @@ declare -a perfcounters
 # results
 declare -a res
 
+perfver=`perf --version`
+perfpipe="2"
+# perf version 3...
+if [[ "$perfver" =~ ^perf\ version\ 2.* ]]; then
+    echo "perf version 2 detected"
+    perfpipe="0"
+fi
 
 function printAvailablePerfCounters {
   echo "Available performance counters are: ${availPerfCounters[@]}"
@@ -237,7 +244,7 @@ function parseOutput {
 # run the program...
 function runProgram {
   echo "running perf stat $PERFARGS $PROGR $INSIZE ..."
-  perfout=`perf stat $PERFARGS $PROGR $INSIZE 2>&1`
+  perfout=`perf stat $PERFARGS $PROGR $INSIZE $perfpipe>&1`
   # parse the output of perf stat
   parseOutput
   echo "$PROGR $INSIZE terminated"
